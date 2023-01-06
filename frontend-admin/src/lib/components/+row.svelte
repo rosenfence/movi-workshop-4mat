ebbadcf1847d112f0023398a5ae20b0c91560ec5
Ming <nohack-@naver.com>
2023-01-07 01:24:05 +0900

    feat: 페이지 기본 마크업

---
+++ frontend-admin/src/lib/components/+row.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  let text: string;
+</script>
+
+<td class="text-gray-700 text-center border select-none">{text}</td>
---
+++ frontend-admin/src/routes/qr/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import QRImage from '$lib/assets/qr.png';
+</script>
+
+<svelte:head>
+  <title>QR 코드</title>
+</svelte:head>
+
+<div class="w-full h-full grid place-items-center">
+  <div class="flex flex-col items-center">
+    <img class="w-11/12 max-w-3xl mb-2" src={QRImage} alt="QR Code" />
+    <h2 class="text-xl font-medium text-center">QR 코드를 스캔해서 접속해 주세요 🙇‍♂️</h2>
+  </div>
+</div>
