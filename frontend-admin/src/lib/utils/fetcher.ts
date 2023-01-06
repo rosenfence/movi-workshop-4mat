c3a0a50f4be610d7815c38d711b20111d68bb481
Ming <nohack-@naver.com>
2023-01-07 03:15:35 +0900

    feat: 테스트 결과 보기 페이지

---
+++ frontend-admin/src/lib/utils/fetcher.ts
@@ -0,0 +1,61 @@
+type DataResponse = {
+  name: string;
+  result: {
+    partA: [number, number, number, number];
+    partB: number;
+  };
+};
+
+export type Data = Omit<DataResponse, 'result'> & {
+  name: string;
+  result: {
+    partA: { value: number; isTop: boolean }[];
+    partB: { value: number; isTop: boolean };
+  };
+};
+
+const fetcher = async () => {
+  const response = await fetch('/api/data');
+  const data: DataResponse[] = await response.json();
+
+  let [a, b, c, d, e] = [0, 0, 0, 0, 0];
+
+  let result = data.map((v) => {
+    const name = v.name;
+    const partA = [...v.result.partA];
+    const partB = v.result.partB;
+
+    [a, b, c, d, e] = [
+      Math.max(a, partA[0]),
+      Math.max(b, partA[1]),
+      Math.max(c, partA[2]),
+      Math.max(d, partA[3]),
+      Math.max(e, partB),
+    ];
+
+    return {
+      name,
+      result: {
+        partA: [
+          { value: partA[0], isTop: false },
+          { value: partA[1], isTop: false },
+          { value: partA[2], isTop: false },
+          { value: partA[3], isTop: false },
+        ],
+        partB: { value: partB, isTop: false },
+      },
+    };
+  });
+
+  result = result.map((v) => ({
+    ...v,
+    result: {
+      partA: v.result.partA.map((v, i) => ({ ...v, isTop: v.value === [a, b, c, d][i] })),
+      partB: { ...v.result.partB, isTop: v.result.partB.value === e },
+    },
+  }));
+
+  return result;
+};
+
+export default fetcher;
--- frontend-admin/src/routes/+page.svelte
+++ frontend-admin/src/routes/+page.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
-  import Cell from '../lib/components/+cell.svelte';
+  import { onMount, onDestroy } from 'svelte';
+  import Cell from '$lib/components/+cell.svelte';
+  import fetcher, { type Data } from '$lib/utils/fetcher';
+
+  let datas: Data[] = [];
+  let timer: NodeJS.Timer;
+
+  onMount(async function () {
+    datas = await fetcher();
+    timer = setInterval(async () => {
+      datas = await fetcher();
+    }, 1000);
+  });
+
+  onDestroy(() => {
+    timer && clearInterval(timer);
+  });
 </script>
 
 <svelte:head>
@@ -24,14 +40,14 @@
       </tr>
     </thead>
     <tbody>
-      {#each Array.from({ length: 10 }) as v, i}
+      {#each datas as data}
         <tr class="h-12">
-          <Cell type="body">새던</Cell>
-          <Cell type="body">1</Cell>
-          <Cell type="body">1</Cell>
-          <Cell type="body">1</Cell>
-          <Cell type="body">d</Cell>
-          <Cell type="body">d</Cell>
+          <Cell type="body">{data.name}</Cell>
+          <Cell type="body" highlight={data.result.partA[0].isTop}>{data.result.partA[0].value}</Cell>
+          <Cell type="body" highlight={data.result.partA[1].isTop}>{data.result.partA[1].value}</Cell>
+          <Cell type="body" highlight={data.result.partA[2].isTop}>{data.result.partA[2].value}</Cell>
+          <Cell type="body" highlight={data.result.partA[3].isTop}>{data.result.partA[3].value}</Cell>
+          <Cell type="body" highlight={data.result.partB.isTop}>{data.result.partB.value}</Cell>
         </tr>
       {/each}
     </tbody>
