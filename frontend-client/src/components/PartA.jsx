50bac3aa83244e6efd0df69fb532d9e37d39361b
Ming <nohack-@naver.com>
2023-01-09 16:55:13 +0900

    feat: Progress 컴포넌트

--- frontend-client/src/components/PartA.jsx
+++ frontend-client/src/components/PartA.jsx
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import strings from '../constants/strings';
 import { Option, Progress } from './';
 
-const PartA = () => {
+const PartA = ({ onChangeResult, totalQuestions }) => {
+  const [questionNumber, setQuestionNumber] = useState(0);
+  const [number, setNumber] = useState(4);
   const [sort, setSort] = useState({
     one: 0,
     two: 0,
     three: 0,
     four: 0,
   });
-  const [number, setNumber] = useState(4);
-  const [questionNumber, setQuestionNumber] = useState(0);
 
   const isValid = () => {
     return Object.keys(sort).every((key) => sort[key] !== 0);
@@ -41,26 +41,37 @@ const PartA = () => {
   // TODO: 모든 문제 완료 시 수고하셧다는 문구 끝.
 
   return (
-    <main className='h-full flex flex-col'>
-      <Progress />
-      <section className='flex-1 flex-col flex-center pb-10'>
-        <div className='flex-center h-[30%] text-gray-700 text-3xl'>PART A</div>
-        <div className='h-[20%] text-gray-700 text-2xl'>본인과 가장 비슷한 질문부터 순서대로 클릭해주세요.</div>
-        <div className='h-[40%]'>문항번호: {strings[questionNumber].title}</div>
+    <main className="h-full flex flex-col">
+      <Progress value={questionNumber} max={totalQuestions} />
+      <section className="flex-1 flex-col flex-center pb-10">
+        <p className="mb-1 text-gray-700 text-xl text-center">
+          PART A ({`${(questionNumber + 1).toString().padStart(2, '0')}`} / {totalQuestions})
+        </p>
+        <p className="mb-4 text-gray-700">본인과 가장 비슷한 질문부터 순서대로 클릭해주세요.</p>
+        <p className="text-2xl font-medium">{strings[questionNumber].title}</p>
       </section>
-      <section className='mb-4'>
-        {['one', 'two', 'three', 'four'].map((option) => (
-          <Option key={option} idx={questionNumber} option={option} number={sort[option]} onClick={sort[option] !== 0 ? undefined : () => handleClickOption(option)} />
-        ))}
-        <div className='flex justify-end'>
-          <button className='py-3 px-5 text-white bg-gray-600 rounded-md transition-colors hover:bg-gray-700' onClick={handleClickReset}>
+      <section className="mb-4">
+        <div className="flex justify-end">
+          <button
+            className="mb-4 py-3 px-5 text-white bg-gray-600 rounded-md transition-colors hover:bg-gray-700"
+            onClick={handleClickReset}
+          >
             초기화
           </button>
         </div>
+        {['one', 'two', 'three', 'four'].map((option) => (
+          <Option
+            key={option}
+            idx={questionNumber}
+            option={option}
+            number={sort[option]}
+            onClick={sort[option] !== 0 ? undefined : () => handleClickOption(option)}
+          />
+        ))}
       </section>
       <section>
         <button
-          className='w-full h-[48px] font-medium bg-orange-400 rounded-md transition-colors hover:bg-orange-500 disabled:text-gray-400 disabled:bg-gray-200'
+          className="w-full h-[48px] font-medium bg-orange-400 rounded-md transition-colors hover:bg-orange-500 disabled:text-gray-400 disabled:bg-gray-200"
           onClick={handleClickNext}
           disabled={!isValid()}
         >
--- frontend-client/src/components/Progress.jsx
+++ frontend-client/src/components/Progress.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Progress = ({ value = 0 }) => {
+const Progress = ({ value = 0, max }) => {
   return (
-    <div className='absolute top-[60px] left-0 w-full h-[12px] bg-gray-100'>
+    <div className="absolute top-[60px] left-0 w-full h-[12px] bg-gray-100">
       <div
         style={{
-          width: `${value}%`,
+          width: `${(value / max) * 100}%`,
         }}
-        className='absolute top-0 left-0 h-full bg-red-500 transition-all'
+        className="absolute top-0 left-0 h-full bg-red-500 transition-all"
       />
     </div>
   );
